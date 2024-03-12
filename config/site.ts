bafb725a3c27b91f0f13b937bb7767f7a3f5cedb
Jonathan Varela <jonvarela4@gmail.com>
2024-03-11 23:32:53 -0400

    Stage 3: Site Header

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: 'DailyBlog',
+  url: 'https://example.com',
+  description: 'Nextjs 14 using velite, tailwind and shadcn',
+  author: 'JollyCoding',
+  links: {
+    twitter: 'https://twitter.com/jollycoding',
+    github: 'https://github.com/jonathan',
+    personalSite: 'https://jonathan.com',
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
