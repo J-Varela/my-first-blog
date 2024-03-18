66121326b8ea43b194e2b4babdd7f35482f0e2a5
Jonathan Varela <jonvarela4@gmail.com>
2024-03-18 00:59:28 -0400

    Stage 10: Pagination

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,22 +1,45 @@
 import { posts } from '#site/content';
 import { PostItem } from '@/components/post-item';
+import { QueryPagination } from '@/components/query-pagination';
 import { sortPosts } from '@/lib/utils';
+import { Metadata } from 'next';
 
-export default function BlogPage() {
+export const metadata: Metadata = {
+  title: 'My blog',
+  description: 'This is a description',
+};
+
+const POSTS_PER_PAGE = 5;
+
+interface BlogPageProps {
+  searchParams: {
+    page?: string;
+  };
+}
+
+export default async function BlogPage({ searchParams }: BlogPageProps) {
+  const currentPage = Number(searchParams?.page) || 1;
   const sortedPosts = sortPosts(posts.filter((post) => post.published));
-  const displayPosts = posts;
+  const totalPages = Math.ceil(sortedPosts.length / POSTS_PER_PAGE);
+
+  const displayPosts = sortedPosts.slice(
+    POSTS_PER_PAGE * (currentPage - 1),
+    POSTS_PER_PAGE * currentPage
+  );
 
   return (
-    <div>
-      <div>
-        <div>
-          <h1>Blog</h1>
-          <p>My ramblings on all things.</p>
+    <div className="container mex-w-4xl py-6 lg:py-10">
+      <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between">
+        <div className="flex-1 space-y-4">
+          <h1 className="inline-block font-black text-4xl lg:text-5xl">Blog</h1>
+          <p className="text-xl text-muted-foreground">
+            My ramblings on all things.
+          </p>
         </div>
       </div>
-      <hr />
+      <hr className="mt-8" />
       {displayPosts?.length > 0 ? (
-        <ul>
+        <ul className="flex flex-col">
           {displayPosts.map((post) => {
             const { slug, date, title, description } = post;
             return (
@@ -32,8 +55,9 @@ export default function BlogPage() {
           })}
         </ul>
       ) : (
-        <p>Nothing to see here</p>
+        <p>Nothing to see here yet</p>
       )}
+      <QueryPagination totalPages={totalPages} className="justify-end mt-4" />
     </div>
   );
 }
